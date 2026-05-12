d7ba3868204b69546937e8399911f79ca2539e98
Leo Tachini <leo1tachini@gmail.com>
2026-05-12 20:46:47 -0300

    arquitetura do projeto

---
+++ AppSaudeFeminina/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace AppSaudeFeminina;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCriarContaClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new CriarContaPage());
+    }
+
+    private async void OnEntrarClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new EntrarPage());
+    }
+}
